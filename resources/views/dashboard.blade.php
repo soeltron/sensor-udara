4134d1272e4d3aadd3bf88afdbe689da8f492039
soeltron <sultonba164@gmail.com>
2026-06-16 15:09:42 +0700

    fix: csrf token fan mode

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -563,7 +563,7 @@
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
                 },
                 body: JSON.stringify({ mode })
             });
