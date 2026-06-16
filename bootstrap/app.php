29f251adf7576828a5c7228cddca7f312b9beeb7
soeltron <sultonba164@gmail.com>
2026-06-16 15:29:17 +0700

    fix: trust proxies railway https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+        ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->validateCsrfTokens(except: [
             'api/*',
         ]);
